68643b75055202db45f8c5b05ed3aeb97f655ffa
SuperMaxime38 <max.cardot.clement@icloud.com>
2025-04-30 23:59:32 +0200

    small changes

--- src/main/java/ntdjl/RL.java
+++ src/main/java/ntdjl/RL.java
@@ -20,6 +20,8 @@ public class RL {
 	
 	int topOneClones, topTwoClones, topThreeClones;
 	
+	int batch;
+	
 	public RL(int[] structure, ActivationFunction fn, int amount) {
 		this.agents = new ArrayList<NN>();
 		this.structure = structure;
@@ -34,10 +36,59 @@ public class RL {
 		this.topOneClones = 3;
 		this.topTwoClones = 2;
 		this.topThreeClones = 1;
+		
+		this.batch = 0;
 	}
 	
 	public void nextGeneration() {
+		NN top1 = null, top2 = null, top3 = null;
+		int score1 = -1, score2 = -1, score3 = -1;
 		
+		for(NN model : scores.keySet()) {
+			int score = scores.get(model);
+			
+			if(score > score1) {
+				top1 = model;
+				score1 = score;
+			} else if(score > score2) {
+				top2 = model;
+				score2 = score;
+			} else if(score > score3) {
+				top3 = model;
+				score3 = score;
+			}
+		}
+
+		List<NN> clonesOne = Genetics.cloneModel(top1, this.topOneClones);
+		List<NN> clonesTwo = Genetics.cloneModel(top2, this.topTwoClones);
+		List<NN> clonesThree = Genetics.cloneModel(top3, this.topThreeClones);
+		
+		for(int a = 1; a < clonesOne.size(); a++) {
+			NN clone = clonesOne.get(a);
+			clone.mutate(0.01);
+			clonesOne.set(a, clone);
+		}
+		
+		for(int a = 1; a < clonesTwo.size(); a++) {
+			NN clone = clonesTwo.get(a);
+			clone.mutate(0.01);
+			clonesTwo.set(a, clone);
+		}
+		
+		for(int a = 1; a < clonesThree.size(); a++) {
+			NN clone = clonesThree.get(a);
+			clone.mutate(0.01);
+			clonesThree.set(a, clone);
+		}
+		
+		this.agents.clear();
+		this.agents.addAll(clonesOne);
+		this.agents.addAll(clonesTwo);
+		this.agents.addAll(clonesThree);
+		
+		this.it = this.agents.iterator();
+		
+		this.batch++;
 	}
 	
 	public List<NN> getAgents() {
@@ -65,33 +116,5 @@ public class RL {
 		this.topOneClones = topOne;
 		this.topTwoClones = topTwo;
 		this.topThreeClones = topThree;
-	}
-	
-	// PRIVATE
-	
-	private void getHighestScoresClones() {
-		NN top1 = null, top2 = null, top3 = null;
-		int score1 = -1, score2 = -1, score3 = -1;
-		
-		for(NN model : scores.keySet()) {
-			int score = scores.get(model);
-			
-			if(score > score1) {
-				top1 = model;
-				score1 = score;
-			} else if(score > score2) {
-				top2 = model;
-				score2 = score;
-			} else if(score > score3) {
-				top3 = model;
-				score3 = score;
-			}
-		}
-
-		List<NN> clonesOne = Genetics.cloneModel(top1, this.topOneClones);
-		List<NN> clonesTwo = Genetics.cloneModel(top2, this.topTwoClones);
-		List<NN> clonesThree = Genetics.cloneModel(top3, this.topThreeClones);
-		
-		// mutate every model (well keep one parent so if the children are dogshit it won't regress)
 	}
 }
